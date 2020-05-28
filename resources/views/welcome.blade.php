69c8639d7d48ca102a7d040d7c1d7c334dbbdc87
Larry Garfield <larry@garfieldtech.com>
2020-05-28 14:52:59 -0500

    Update to latest upstream

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Test 12
+                    Laravel
                 </div>
 
                 <div class="links">
